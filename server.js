f712e41e42c62905325d021f6319bea4234c48f2
Dave <bbec0689@gmail.com>
2021-03-21 19:24:42 -0600

    file structure and basic code added

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const PORT = process.env.PORT || 3030
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.use(require("./routes"));
+
+app.listen(PORT, () => console.log(`Live on port ${PORT}`));
